13c9ee0970c73e9309973c551da52675ab7d8772
Abid Habib ✅ <41414028+abidhabib@users.noreply.github.com>
2022-05-26 15:33:32 +0500

    Add files via upload

---
+++ TermFolio/js/data.js
@@ -0,0 +1,27 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Ahsan Khan'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Abid Habib'},
+    {name: 'Email', value: 'abid.habib@outlook.com'},
+    {name: 'Phone', value: '+92 333 8053324'},
+    {name: 'Country', value: 'Pakistan'},
+    {name: 'City', value: 'Bhakkar'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'abidhabib'}
+  ],
+  projects: [
+    {name: 'Hackintosh Opencore', value: 'https://github.com/abidhabib/BigSur-9480m-OC-EFI'},
+    {name: 'Windows Activatior Batch Script', value: 'https://github.com/abidhabib/Windows-Activatior-'},
+    {name: 'Open Source Information Facebook (info gathering)', value: 'https://github.com/abidhabib/FEXS'},
+] ,
+  resume: 'ABID_HABIB.pdf',
+  socials: [
+    
+    {name: 'GitHub', value: 'https://github.com/abidhabib'},
+    {name: 'Instagram', value: 'https://instagram/abid._.habib'},
+    {name: 'Website', value: 'itsabid.cf'}
+  ]
+}
